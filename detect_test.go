fab5c97aacaff39e1e0165640c93160eb2cbd35e
zhaowei <zhaowei01@bilibili.com>
2021-04-10 14:20:08 +0800

    test

--- detect_test.go
+++ detect_test.go
@@ -1,11 +1,33 @@
 package hotDetect
 
 import (
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 
 
-func Test_TopN(t *testing.T) {
+func Test_FreqCount(t *testing.T) {
 
+	Convey("Test-FreqCount", t, func() {
+		fc := newFc(4,"test")
+		fc.Add("ID1", 1)
+		fc.Add("ID1", 1)
+		fc.Add("ID1", 1)
+		fc.Add("ID2", 1)
+		fc.Add("ID2", 1)
+		fc.Add("ID3", 1)
+		fc.Add("ID3", 1)
+		fc.Add("ID3", 1)
+		fc.Add("ID3", 1)
+
+		So(len(fc.fu), ShouldEqual, 3)
+		fc.Add("ID4", 1)
+		So(fc.size, ShouldEqual, 3)
+
+		tops := fc.TopKey(2)
+		So(tops[0].Key, ShouldEqual, "ID3")
+		So(tops[1].Key, ShouldEqual, "ID1")
+
+	})
 }
